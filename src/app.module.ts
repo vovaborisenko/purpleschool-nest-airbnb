85ff08f9edb85e28abd63f2772c062af3322dd43
Uladzimir Barysenka <vovaborisenko@live.com>
2025-01-08 19:20:40 +0300

    adds modules room, record

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomModule } from './room/room.module';
+import { RecordModule } from './record/record.module';
 
 @Module({
-  imports: [],
+  imports: [RoomModule, RecordModule],
   controllers: [AppController],
   providers: [AppService],
 })
